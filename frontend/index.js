61cba9a492e23668e22f9a02364d4bf24fd59bb2
souravsingh1903 <souravsingh1903@gmail.com>
2024-03-02 13:22:18 +0530

    frontend sign up page

---
+++ frontend/index.js
@@ -0,0 +1,22 @@
+function signUp(event){
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    const signUp ={
+        name,
+        email,
+        password
+    }
+
+    axios.post("http://localhost:8000/user/sign-up", signUp)
+    .then(response =>{
+        console.log(response);
+    }).catch((e) => {
+        console.log(e);
+    })
+
+
+}
